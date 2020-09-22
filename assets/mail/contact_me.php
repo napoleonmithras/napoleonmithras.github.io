4b0198a130c8cbc2f634556d617e0ced66acd7e0
Napoleon Mithras <knnmithras@gmail.com>
2020-09-23 01:27:05 +0300

    Update contact_me.php

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -16,9 +16,7 @@ $subject = "Website Contact Form:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
 $header = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";	
-$result = mail($to,$email_subject,$email_body,$headers);
-die($result);
 
-// if(!mail($to, $subject, $body, $header))
-//   http_response_code(500);
+if(!mail($to, $subject, $body, $header))
+  http_response_code(500);
 ?>
